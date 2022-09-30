d3631bd0012d19e81baa16b248baae35992f80fe
Sudeep-K <pong32btl@gmail.com>
2022-09-30 10:14:40 +0545

    Completes exercies 1.4

--- part1/course-information/src/components/Content.jsx
+++ part1/course-information/src/components/Content.jsx
@@ -4,9 +4,7 @@ import Part from './Part'
 const Content = (props) => {
   return (
     <>
-        <Part part={props.part1.name} exercises={props.part1.exercises} />
-        <Part part={props.part2.name} exercises={props.part2.exercises} />
-        <Part part={props.part3.name} exercises={props.part3.exercises} />
+      {props.parts.map((part) => <Part name={part.name} exercises={part.exercises} />)}
     </>
   )
 }
--- part1/course-information/src/components/Total.jsx
+++ part1/course-information/src/components/Total.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {parseInt(props.exercises1) + parseInt(props.exercises2) + parseInt(props.exercises3)}</p>
+    <p>Number of exercises {parseInt(props.parts[0].exercises) + parseInt(props.parts[1].exercises) + parseInt(props.parts[2].exercises)}</p>
   )
 }
 
