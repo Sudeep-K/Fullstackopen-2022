5030c383844ee6f3e7d1271bd81f48ed1a94ddd6
Sudeep-K <pong32btl@gmail.com>
2022-09-30 09:55:00 +0545

    Completes exercise 1.3 by  refactoring the application

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -4,26 +4,29 @@ import Total from './components/Total'
 import Content from './components/Content'
 
 function App() {
-  const [course, setCourse] = useState('Half Stack application development')
-  const [part1, setPart1] = useState('Fundamentals of React')
-  const [exercises1, setExercises1] = useState(10);
-  const [part2, setPart2] = useState('Using props to pass data')
-  const [exercises2, setExercises2] = useState(7);
-  const [part3, setPart3] = useState('State of a component')
-  const [exercises3, setExercises3] = useState(14);
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header course={course} />
       <Content 
       part1={part1}
-      exercises1={exercises1} 
       part2={part2}
-      exercises2={exercises2}
       part3={part3}
-      exercises3={exercises3}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
     </>
   );
 }
