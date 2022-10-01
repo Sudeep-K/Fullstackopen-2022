e5c0ce39d2d2bc30ed2bfee6bd7a2f6ae842453f
Sudeep-K <pong32btl@gmail.com>
2022-10-01 15:26:42 +0545

    Completes assignment 1.11 - 1.14 anecdote react app

---
+++ part1/anecdotes/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Button from "./components/Button";
+
+function App() {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0);
+
+  const [points, setPoints] = useState([0, 0, 0, 0, 0, 0, 0]);
+
+  const nextSelection = () => {
+    if (selected < 6) {
+      setSelected(selected + 1)
+    } else {
+      setSelected(0)
+    }
+  }
+
+  const voteSelection = () => {
+    const copy_points = [...points]
+    copy_points[selected] += 1
+    setPoints(copy_points)
+  }
+
+  const mostVotedanecdote = () => {
+    const max = Math.max(...points);
+    const index = points.indexOf(max);
+    return index;
+  }
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has <strong>{points[selected]}</strong> votes</p>
+      <Button text={'vote'} onClick={voteSelection} />
+      <Button text={'next anecdote'} onClick={nextSelection} />
+
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotedanecdote()]}</p>
+      <p>has <strong>{points[mostVotedanecdote()]}</strong> votes</p>
+    </>
+  );
+}
+
+export default App;
