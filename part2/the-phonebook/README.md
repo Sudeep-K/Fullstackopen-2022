170fd85156768318b1107a28b27e85539e641f60
Sudeep-K <pong32btl@gmail.com>
2022-10-08 21:33:05 +0545

    creates a json-server at port:3001 and fetches data via axios module and serves to the app

--- part2/the-phonebook/README.md
+++ part2/the-phonebook/README.md
@@ -1,3 +1,9 @@
 ## the-phonebook
 
-🚧👷‍♀️work in progress
+✅ step 1: npm install
+✅ step 2: npm run server
+✅ step 3: npm start
+
+now, the app is served to you, 
+🅰 filter your contacts
+🅱 add to your contacts
--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
 import Display from "./components/Display";
 import Form from "./components/Form";
 import Filter from "./components/Filter";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [contacts, setContacts] = useState([])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filterInput, setFilterInput] = useState('');
   const [filteredContacts, setFilteredContacts] = useState([]);
+
+  const hook = () => {
+    const eventHandler = (response) => {
+      setContacts(response.data);
+    }
+
+    const promise = axios.get('http://localhost:3001/persons');
+    promise.then(eventHandler);
+  }
+
+  useEffect(hook, []);
 
   const filterContacts = (event) => {
     setFilterInput(event.target.value);
