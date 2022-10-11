cb0ac1ceb0de0d98ccdcb9778c00c0185f835a59
Sudeep-K <pong32btl@gmail.com>
2022-10-11 11:03:12 +0545

    Creates a new notes.js service for all the axios server fetch and put requests

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,7 +1,8 @@
 import Note from "./components/Note";
 import { useState } from "react";
 import { useEffect } from "react";
-import axios from "axios";
+
+import noteServices from './services/notes'
 
 function App() {
   const [notes, setNotes] = useState([]);
@@ -11,12 +12,11 @@ function App() {
   const hook = () => {
     console.log('effect');
 
-    const eventHandler = response => {
-      console.log('promise fulfilled');
-      setNotes(response.data);
+    const eventHandler = initialNotes => {
+      setNotes(initialNotes);
     }
 
-    const promise = axios.get('http://localhost:3001/notes')
+    const promise = noteServices.getAll()
     promise.then(eventHandler);
   }
   
@@ -26,22 +26,36 @@ function App() {
     event.preventDefault();
     
     const noteObject = {
-      id: notes.length + 1,
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5
     }
 
-    setNotes([...notes, noteObject]);
-    setNewNote('');
+    noteServices.create(noteObject)
+    .then(returnedNote => {
+      setNotes([...notes, returnedNote]);
+      setNewNote('');
+    })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value);
   }
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const toogleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id);
+    const changedNote = {...note, important: !note.important};
+
+    noteServices.update(id, changedNote).then(returnedNote => {
+      setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+    })
+    .catch(error => {
+      alert(`the note ${note.content} was already deleted from the server`);
+      setNotes(notes.filter(n => n.id !== id))
+    })
+  }
 
   return (
     <div>
@@ -51,7 +65,7 @@ function App() {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={toogleImportanceOf}/>
         )}
       </ul>
       <form onSubmit={addNote}>
