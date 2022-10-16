3244e9b7f96879223decb654eba09f7f09fff150
Sudeep-K <pong32btl@gmail.com>
2022-10-16 17:04:56 +0545

    gets info about the persons, gets data of individual person and deletes the individual person according to the id of that person

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,12 +1,48 @@
 const express = require("express");
-const persons = require("./persons");
+let persons = require("./persons");
 
 const app = express();
 
+// get all persons
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 })
 
+// get info of the request
+app.get('/info', (req, res) => {
+    const personsCount = persons.length;
+    const date = new Date();
+
+    res.send(`<div>
+                <h3>Phonebook has info for ${personsCount} people</h3>
+                <h3>${date}</h3>
+            </div>`)
+})
+
+// get individual person
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const person = persons.find(p => p.id === id);
+
+    if (!person) {
+        return res.status(404).send();
+    }
+
+    res.json(person);
+})
+
+// delete individual person
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    if (!persons.find(p => p.id === id)) {
+        return res.status(404).send();
+    }
+
+    persons = persons.filter(p => p.id !== id);
+    res.status(204).end();
+})
 
 const PORT = 5000;
 app.listen(PORT, () => {
