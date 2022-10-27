2b7eec70c740a8c49e0a57c6677dd132a3c8b976
Sudeep-K <pong32btl@gmail.com>
2022-10-27 13:48:23 +0545

    configures database to its own module and uses database in route handler

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,44 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const Note = require("./models/note");
 
 const app = express();
 
-// middleware
-const requestLogger = (req, res, next) => {
-    console.log('Method: ', req.method);
-    console.log('Path: ', req.path);
-    console.log('Body: ', req.body);
-    console.log('--');
-    next();
-}
+// // middleware
+// const requestLogger = (req, res, next) => {
+//     console.log('Method: ', req.method);
+//     console.log('Path: ', req.path);
+//     console.log('Body: ', req.body);
+//     console.log('--');
+//     next();
+// }
 
 app.use(express.static('build'));
 app.use(express.json());
-morgan.token('body', (req, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content] - :response-time ms :body'));
-app.use(requestLogger);
+// morgan.token('body', (req, res) => JSON.stringify(req.body));
+// app.use(morgan(':method :url :status :res[content] - :response-time ms :body'));
+// app.use(requestLogger);
 
-
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-    }
-]
+// let notes = [
+//     {
+//         id: 1,
+//         content: "HTML is easy",
+//         date: "2022-05-30T17:30:31.098Z",
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: "Browser can execute only Javascript",
+//         date: "2022-05-30T18:39:34.091Z",
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: "GET and POST are the most important methods of HTTP protocol",
+//         date: "2022-05-30T19:20:14.298Z",
+//         important: true
+//     }
+// ]
 
 const generateId = () => {
     const maxId = notes.length > 0 ? 
@@ -47,22 +48,23 @@ const generateId = () => {
     return maxId + 1;
 }
 
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 })
 
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    Note.find({})
+    .then(notes => {
+        res.json(notes);
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const note = notes.find(n => n.id === id);
-    if (note) {
+    Note.findById(req.params.id)
+    .then(note => {
         res.json(note);
-    } else {
-        res.status(404).end();
-    }  
+    }) 
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -75,21 +77,20 @@ app.delete('/api/notes/:id', (req, res) => {
 app.post('/api/notes', (req, res) => {
     const body = req.body;
 
-    // if (!body.header) {
-    //     return res.status(400).json({
-    //         error: 'content missing'
-    //     })
-    // }
+    if (body.content === undefined) {
+        return res.status(400).json({ error: 'content missing' });
+    }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
-        id: generateId()
-    }
+    })
 
-    notes.concat(note);
-    res.json(note);
+    note.save()
+    .then(savedNote => {
+        res.json(savedNote);
+    })
 })
 
 const unknownEndPoint = (req, res, next) => {
@@ -98,7 +99,7 @@ const unknownEndPoint = (req, res, next) => {
 
 app.use(unknownEndPoint);
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
