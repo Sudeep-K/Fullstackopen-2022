f4e9d1307aff3b5ca9a5b997c84e7c1fa7614974
Sudeep-K <pong32btl@gmail.com>
2022-11-05 11:54:28 +0545

    Refactor the application into separate modules

--- part3/notes-backend/models/note.js
+++ part3/notes-backend/models/note.js
@@ -1,17 +1,5 @@
 const mongoose = require("mongoose");
 
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose.connect(url)
-.then(result => {
-    console.log("connected to MongoDB");
-})
-.catch(err => {
-    console.log("error connecting to MongoDB:", err.message);
-})
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
@@ -33,4 +21,4 @@ noteSchema.set('toJSON', {
     }
 })
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema);
