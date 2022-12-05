58ed4b080bee21001f8c8c924e82b6de72395315
Sudeep-K <pong32btl@gmail.com>
2022-12-05 08:38:34 +0545

    adds useCountry and configures the hook for the application

---
+++ part7/country-hook/src/hooks/index.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+  
+    const onChange = (event) => {
+      setValue(event.target.value)
+    }
+  
+    return {
+      type,
+      value,
+      onChange
+    }
+}
+  
+export const useCountry = (name) => {
+    const [country, setCountry] = useState(null)
+  
+    useEffect(() => {
+        axios.get(`https://restcountries.com/v3.1/name/${name}`)
+        .then(res => setCountry(res.data[0]))
+    }, [country])
+  
+    return country
+}
